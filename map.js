ebe6dfdc4763c367d15bf6c8c2e0fecb8682a883
c03rcion <areeves9@icloud.com>
2016-03-17 17:48:29 -0500

    added custom style to map object

--- map.js
+++ map.js
@@ -1,15 +1,30 @@
 var loc = [];
 var strainloc = [];
-
 // initialize map object
 function initialize() {
+  var customMapType = new google.maps.StyledMapType(
+
+  [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}],
+     {
+      name: 'Custom Style'
+
+  });
+
+  var customMapTypeId = 'custom_style';
   var myOptions = {
     zoom: 10,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    mapTypeControlOptions: {
+        mapTypeIds: [ google.maps.MapTypeId.ROADMAP, customMapTypeId ]
+    }
   };
 
-  var geocoder = new google.maps.Geocoder();
+
+
   var map = new google.maps.Map(document.getElementById("map"), myOptions);
+  map.mapTypes.set(customMapTypeId, customMapType);
+  map.setMapTypeId(customMapTypeId);
+
+  var geocoder = new google.maps.Geocoder();
   var infowindow = new google.maps.InfoWindow({
     maxWidth: 150
   });
